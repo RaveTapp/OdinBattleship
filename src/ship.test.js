bf7657e6a8ca8bc0fedb95a30a35b5cb6d78ad5e
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-07-10 17:51:08 +0200

    Make ship's properties private

--- src/ship.test.js
+++ src/ship.test.js
@@ -4,7 +4,7 @@ describe("Ship receiving hits", () => {
     test("Register 1 hit", () => {
         let testShip = new ship(4);
         testShip.hit();
-        expect(testShip.beenHit).toBe(1);
+        expect(testShip.getNumOfHits()).toBe(1);
     })
 
     test("More hits than ship length", () => {
@@ -12,7 +12,7 @@ describe("Ship receiving hits", () => {
         for(let i = 0; i < 5; i++){
             testShip.hit();
         }
-        expect(testShip.beenHit).toBe(3);
+        expect(testShip.getNumOfHits()).toBe(3);
     })
 
     test("Ship is originaly not sunk", () => {
@@ -32,6 +32,6 @@ describe("Ship receiving hits", () => {
             testShip.hit();
         }
         expect(testShip.isSunk()).toBeTruthy();
-        expect(testShip.beenHit).toBe(5);
+        expect(testShip.getNumOfHits()).toBe(5);
     })
 });
